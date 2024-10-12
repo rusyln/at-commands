21df01481df6c3b91ce6c09ef67cde4dff4d309a
rusyln <31722099+rusyln@users.noreply.github.com>
2024-10-12 08:59:52 +0800

    fix

--- capstone.py
+++ capstone.py
@@ -224,8 +224,8 @@ def start_bluetooth():
                     print("Ready to execute the Raspberry Pi command...")
                     run_raspberry_pi_command("sudo sdptool add --channel=24 SP")
                     print("Command executed successfully.")
-                    time.sleep(5)  # Wait for 5 seconds
-                    start_rfcomm_server()  # Start the RFCOMM server
+                    #time.sleep(5)  # Wait for 5 seconds
+                    #start_rfcomm_server()  # Start the RFCOMM server
                     
 
     except KeyboardInterrupt:
